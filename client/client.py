fe5dc0288799c1f3d97b3d2c089e10802ffbff72
Bartosz Piotr Lachowicz <bllachow@amazon.com>
2021-09-23 16:04:58 +0200

    Added file sending

--- client/client.py
+++ client/client.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 
 import socket
+import os
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 65439        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    for pk in range (0, 10):
-        pkg = str.encode(f'package{pk}')
-        s.sendall(pkg)
-        data = s.recv(1024)
-        print('Received', repr(data))
+filename = 'client_files/text1.txt'
+file_size = os.path.getsize(filename)
+print(file_size)
+BUFFER_SIZE = 200
+SEPARATOR = '<SEPARATOR>'
 
+def send_file(filename, file_size, host=HOST, port=PORT):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.send(f"{filename}{SEPARATOR}{file_size}".encode('utf-8'))
+            i = 0
+            with open(filename) as f:
+                while True:
+                    print(f'iter: {i}')
+                    bytes_read = f.read(BUFFER_SIZE).encode('utf-8')
+                    
+                    if not bytes_read:
+                        break
+
+                    s.sendall(bytes_read)
+                    i += 1
+            data = s.recv(1024)
+
+send_file(filename=filename, file_size=file_size)
--- server/server.py
+++ server/server.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 import socket
+import os
+from datetime import datetime
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65439        # Port to listen on (non-privileged ports are > 1023)
+SEPARATOR = '<SEPARATOR>'
+BUFFER_SIZE = 200
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -11,10 +16,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f'I JUST RECEIVED FOLLOWING DATA:{data.decode("utf-8")}')
-            result = str.encode("server " + data.decode('utf-8'), 'utf-8')
-            conn.sendall(result)
+
+        data = conn.recv(BUFFER_SIZE).decode('utf-8')
+        filename, filesize = data.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+
+        with open(os.path.join('server_files',filename), "wb") as f:
+            i = 0 
+            
+            while True:
+                print(f'iter: {i}')
+                bytes_read = conn.recv(BUFFER_SIZE)
+                print(repr(bytes_read))
+                if not bytes_read:
+                    break
+                f.write(bytes_read)
+                i += 1
+        conn.sendall(bytes('siema', 'u'))
+        print('end of conn')
+
+    print('123')
